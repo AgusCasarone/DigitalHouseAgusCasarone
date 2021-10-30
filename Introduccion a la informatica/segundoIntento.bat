b4ddb2639ac61528b9de672cdfb890f853ac9478
AgusCasarone <agus.casarone@gmail.com>
2021-10-30 18:24:31 -0300

    toda la carpeta de Digital House

---
+++ Introduccion a la informatica/segundoIntento.bat
@@ -0,0 +1,28 @@
+cd C:/Users/Agus/Desktop
+
+rm -R dh
+
+mkdir DH
+
+cd dh
+
+mkdir peliculas
+
+cd peliculas
+
+mkdir accion comedia infantiles terror romantica
+
+cd accion
+touch "mision imposible" "rapido y furioso" "los vengadores"
+
+cd ../comedia
+touch "que paso ayer" "la mascara"
+
+cd ../infantiles
+touch cars madagascar "la dama y el vagabundo" pocahontas
+
+cd ../terror
+touch "el exorcista" it "el juego del miedo" annabelle
+
+cd ../romantica
+touch "diario de una pasion" "orgullo y prejuicio" titanic "yo antes de ti"
